5a85bbaaee187d38a165d46ea6a75b68d3abf83e
yihhao wang <2633565580@qq.com>
2024-04-04 12:53:31 +0800

    docs(examples): update metarouter examples

--- examples/metarouter/main.go
+++ examples/metarouter/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/246859/ginx"
+	"github.com/gin-gonic/gin"
+	"log"
 	"log/slog"
 )
 
@@ -11,9 +13,19 @@ func main() {
 	root := server.RouterGroup()
 	root.MGET("login", ginx.M{{"role", "guest"}, {"limit", 5}})
 	user := root.MGroup("user", nil)
-	user.MGET("info", ginx.M{{"role", "user"}}, nil)
+	user.MGET("info", ginx.M{{"role", "user"}}, func(ctx *gin.Context) {
+		// get metadata from context
+		metaData := ginx.MetaFromCtx(ctx)
+		slog.Info(metaData.ShouldGet("role").String())
+	})
 
+	// walk root router
 	root.Walk(func(info ginx.RouteInfo) {
 		slog.Info(fmt.Sprintf("%s %s", info.FullPath, info.Meta))
 	})
+
+	err := server.Spin()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
