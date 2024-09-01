b1031ba65e077dd424a47840ffb3664236d3b526
246859 <2633565580@qq.com>
2024-09-01 14:42:56 +0800

    feat(validate): support humanized validator

--- validate.go
+++ validate.go
@@ -1,30 +1,97 @@
 package ginx
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
-	"github.com/ginx-contribs/ginx/constant/status"
 	"github.com/ginx-contribs/ginx/pkg/resp"
+	localeen "github.com/go-playground/locales/en"
+	unitrans "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
+	transen "github.com/go-playground/validator/v10/translations/en"
+	"reflect"
+	"strings"
 )
+
+// Validator is wrapper of binding.StructValidator
+type Validator interface {
+	binding.StructValidator
+	// HandleError judge that how to handle params validation errors
+	HandleError(ctx *gin.Context, val any, err error)
+}
+
+func NewHumanizedValidator(v *validator.Validate, translator unitrans.Translator) *HumanizedValidator {
+	return &HumanizedValidator{v: v, translator: translator}
+}
+
+// HumanizedValidator return human-readable validation result information
+type HumanizedValidator struct {
+	translator unitrans.Translator
+	v          *validator.Validate
+}
+
+func (h *HumanizedValidator) HandleError(ctx *gin.Context, val any, err error) {
+	var validationErrors validator.ValidationErrors
+	if errors.As(err, &validationErrors) {
+		var errorMsg []string
+		for _, validateErr := range validationErrors {
+			errorMsg = append(errorMsg, validateErr.Translate(h.translator))
+		}
+		resp.Fail(ctx).ErrorMsg(strings.Join(errorMsg, ",")).JSON()
+		return
+	}
+	resp.Fail(ctx).ErrorMsg("params validate failed").JSON()
+}
+
+func (h *HumanizedValidator) ValidateStruct(a any) error {
+	return h.v.Struct(a)
+}
+
+func (h *HumanizedValidator) Engine() any {
+	return h.v
+}
+
+// SetValidator replace the default validator for binding packages
+func SetValidator(structValidator binding.StructValidator) {
+	binding.Validator = structValidator
+}
+
+// EnglishValidator create a validator can return human-readable parameters validation information with language english.
+func EnglishValidator(v *validator.Validate) (*HumanizedValidator, error) {
+	localeEn := localeen.New()
+	universalTranslator := unitrans.New(localeEn)
+	enTrans, _ := universalTranslator.GetTranslator(localeEn.Locale())
+	err := transen.RegisterDefaultTranslations(v, enTrans)
+	if err != nil {
+		return nil, err
+	}
+	v.RegisterTagNameFunc(func(field reflect.StructField) string {
+		lookupNames := []string{"label", "form", "uri", "json", "yaml", "toml", "xml", "toml", "header"}
+		for _, tag := range lookupNames {
+			if name, ok := field.Tag.Lookup(tag); ok {
+				return name
+			}
+		}
+		return field.Name
+	})
+	return NewHumanizedValidator(v, enTrans), nil
+}
 
 // ValidateHandler will be called if validate failed.
 type ValidateHandler func(ctx *gin.Context, val any, err error)
 
-var DefaultValidateHandler ValidateHandler = func(ctx *gin.Context, val any, err error) {
-	response := resp.Fail(ctx).Error(err)
-	if _, ok := err.(validator.ValidationErrors); ok {
-		response = response.Msg("invalid parameters")
-	} else {
-		response = response.Status(status.InternalServerError).Msg(status.InternalServerError.String())
-	}
-	response.JSON()
+var defaultValidateHandler ValidateHandler
+
+func SetValidateHandler(handler ValidateHandler) {
+	defaultValidateHandler = handler
 }
 
 func ShouldValidateWith(ctx *gin.Context, val any, binding binding.Binding) error {
 	err := ctx.ShouldBindWith(val, binding)
 	if err != nil {
-		DefaultValidateHandler(ctx, val, err)
+		if defaultValidateHandler != nil {
+			defaultValidateHandler(ctx, val, err)
+		}
 		return err
 	}
 	return nil
@@ -33,16 +100,20 @@ func ShouldValidateWith(ctx *gin.Context, val any, binding binding.Binding) erro
 func ShouldValidate(ctx *gin.Context, val any) error {
 	err := ctx.ShouldBind(val)
 	if err != nil {
-		DefaultValidateHandler(ctx, val, err)
+		if defaultValidateHandler != nil {
+			defaultValidateHandler(ctx, val, err)
+		}
 		return err
 	}
 	return nil
 }
 
 func ShouldValidateJSON(ctx *gin.Context, val any) error {
-	err := ctx.ShouldBind(val)
+	err := ctx.ShouldBindJSON(val)
 	if err != nil {
-		DefaultValidateHandler(ctx, val, err)
+		if defaultValidateHandler != nil {
+			defaultValidateHandler(ctx, val, err)
+		}
 		return err
 	}
 	return nil
@@ -51,7 +122,9 @@ func ShouldValidateJSON(ctx *gin.Context, val any) error {
 func ShouldValidateQuery(ctx *gin.Context, val any) error {
 	err := ctx.ShouldBindQuery(val)
 	if err != nil {
-		DefaultValidateHandler(ctx, val, err)
+		if defaultValidateHandler != nil {
+			defaultValidateHandler(ctx, val, err)
+		}
 		return err
 	}
 	return nil
@@ -60,7 +133,9 @@ func ShouldValidateQuery(ctx *gin.Context, val any) error {
 func ShouldValidateURI(ctx *gin.Context, val any) error {
 	err := ctx.ShouldBindUri(val)
 	if err != nil {
-		DefaultValidateHandler(ctx, val, err)
+		if defaultValidateHandler != nil {
+			defaultValidateHandler(ctx, val, err)
+		}
 		return err
 	}
 	return nil
@@ -69,7 +144,9 @@ func ShouldValidateURI(ctx *gin.Context, val any) error {
 func ShouldValidateHeader(ctx *gin.Context, val any) error {
 	err := ctx.ShouldBindHeader(val)
 	if err != nil {
-		DefaultValidateHandler(ctx, val, err)
+		if defaultValidateHandler != nil {
+			defaultValidateHandler(ctx, val, err)
+		}
 		return err
 	}
 	return nil
@@ -78,7 +155,9 @@ func ShouldValidateHeader(ctx *gin.Context, val any) error {
 func ShouldValidateXML(ctx *gin.Context, val any) error {
 	err := ctx.ShouldBindXML(val)
 	if err != nil {
-		DefaultValidateHandler(ctx, val, err)
+		if defaultValidateHandler != nil {
+			defaultValidateHandler(ctx, val, err)
+		}
 		return err
 	}
 	return nil
@@ -87,7 +166,9 @@ func ShouldValidateXML(ctx *gin.Context, val any) error {
 func ShouldValidateYAML(ctx *gin.Context, val any) error {
 	err := ctx.ShouldBindYAML(val)
 	if err != nil {
-		DefaultValidateHandler(ctx, val, err)
+		if defaultValidateHandler != nil {
+			defaultValidateHandler(ctx, val, err)
+		}
 		return err
 	}
 	return nil
@@ -96,7 +177,9 @@ func ShouldValidateYAML(ctx *gin.Context, val any) error {
 func ShouldValidateTOML(ctx *gin.Context, val any) error {
 	err := ctx.ShouldBindTOML(val)
 	if err != nil {
-		DefaultValidateHandler(ctx, val, err)
+		if defaultValidateHandler != nil {
+			defaultValidateHandler(ctx, val, err)
+		}
 		return err
 	}
 	return nil
